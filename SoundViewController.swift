30a82c6ae36ab5f6f12c6a1f5fc8d92ef2032ccb
olgagian <ioannis@poleiseis.giannakidis.local>
2017-01-31 14:30:42 +0200

    iboutlets

---
+++ SoundViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by ioannis giannakidis on 31/01/2017.
+//  Copyright © 2017 ioannis giannakidis. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func recordTapped(_ sender: Any) {
+        
+    }
+    @IBAction func playTapped(_ sender: Any) {
+    
+    }
+    @IBAction func AddTapped(_ sender: Any) {
+    
+    }
+
+}
+
